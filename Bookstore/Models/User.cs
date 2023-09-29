e1f690a7946cb2b0750ff8e35ced3ca6a83fb6ed
DonKihot <donkihot1984@gmail.com>
2023-09-29 22:33:19 +0300

    models creating started

---
+++ Bookstore/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+
+    [Index("Name", IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
